501f16201543352ea611ec0566c7356658e8e038
Ahmad Usman <usmanah9817@gmail.com>
2020-08-12 22:42:00 +0100

    adjust nav

--- shared/sharedscript.js
+++ shared/sharedscript.js
@@ -100,12 +100,13 @@ const myAccountBtnHandler = () => {
 
 const closeAccountNavHandler = () => {
     console.log('here2');
+    if (sidenav.mobileNav.contains(sidenav.myAccountNav)) {
+      sidenav.mobileNav.replaceChild(sidenav.pagesNav, sidenav.myAccountNav);
+      return
+    }
     sidenav.sideNavNav.style.display = 'block';
     sidenav.myAccountNav.classList.remove('open-account-nav')
     sidenav.nav.style.height = '18rem'
-    // if (sidenav.mobileNav.contains(sidenav.myAccountNav)) {
-    //   sidenav.mobileNav.replaceChild(sidenav.pagesNav, sidenav.myAccountNav);
-    // }
 }
 
 const mobileNavHandler = () => {
