9fe349d7fe0c584cd3db79a44700f11fe21325c5
Ahmad Usman <usmanah9817@gmail.com>
2020-09-02 00:40:46 +0100

    more responsiveness

--- assets/script/sharedscript.js
+++ assets/script/sharedscript.js
@@ -1,6 +1,6 @@
 const DOMElements = {
   nav: document.querySelector(".side-nav"),
-  mobileNav: document.querySelector('.mobile-nav'),
+  mobileNav: document.querySelector(".mobile-nav"),
   navIcon: document.getElementById("nav-icon"),
   closeNavBtn: document.querySelector(".close-nav-btn"),
   overlay: document.getElementById("overlay"),
@@ -9,13 +9,13 @@ const DOMElements = {
   myAccountNav: document.querySelector(".my-account-nav"),
   mobileNavAccount: document.querySelector(".mobile-nav__myaccount"),
   pagesNav: document.querySelector(".pages-nav"),
-  closeAccountNav: document.querySelector('.close-account-nav'),
+  closeAccountNav: document.querySelector(".close-account-nav"),
   indicators: document.querySelectorAll(".indicator"),
   slides: document.querySelectorAll(".carousel-item"),
+  carouselContainer: document.querySelector(".carousel"),
   activeCarousel: document.querySelector(".carousel-item.active"),
   activeIndicator: document.querySelector(".indicator.active"),
-  orderBtn: document.querySelectorAll(".order-btn")
-
+  orderBtn: document.querySelectorAll(".order-btn"),
 };
 
 // Make navbar sticky
@@ -41,7 +41,6 @@ console.log(DOMElements.activeCarousel);
 var currentSlide = 1;
 let interval;
 
-
 function showSlide(slideIndex, clicked) {
   console.log(currentSlide);
   if (currentSlide >= DOMElements.slides.length) {
@@ -63,6 +62,70 @@ function showSlide(slideIndex, clicked) {
   currentSlide++;
 }
 
+var xDown = null;
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches;
+}
+
+function handleTouchStart(evt) {
+  const firstTouch = getTouches(evt)[0];
+  xDown = firstTouch.clientX;
+  yDown = firstTouch.clientY;
+}
+
+function handleTouchMove(evt) {
+  if (!xDown || !yDown) {
+    return;
+  }
+
+  var xUp = evt.touches[0].clientX;
+  var yUp = evt.touches[0].clientY;
+
+  var xDiff = xDown - xUp;
+  var yDiff = yDown - yUp;
+
+  if (Math.abs(xDiff) > Math.abs(yDiff)) {
+    /*most significant*/
+    if (xDiff > 0) {
+      /* left swipe */
+      currentSlide--;
+      DOMElements.activeCarousel.classList.remove("active");
+      DOMElements.activeIndicator.classList.remove("active");
+    
+      DOMElements.slides[currentSlide].classList.add("active");
+      DOMElements.indicators[currentSlide].classList.add("active");
+    
+      DOMElements.activeCarousel = DOMElements.slides[currentSlide];
+      DOMElements.activeIndicator = DOMElements.indicators[currentSlide];
+    
+    } else {
+      /* right swipe */
+      currentSlide++;
+      
+      DOMElements.activeCarousel.classList.remove("active");
+      DOMElements.activeIndicator.classList.remove("active");
+    
+      DOMElements.slides[currentSlide].classList.add("active");
+      DOMElements.indicators[currentSlide].classList.add("active");
+    
+      DOMElements.activeCarousel = DOMElements.slides[currentSlide];
+      DOMElements.activeIndicator = DOMElements.indicators[currentSlide];
+    
+    }
+  } else {
+    if (yDiff > 0) {
+      /* up swipe */
+    } else {
+      /* down swipe */
+    }
+  }
+  /* reset values */
+  xDown = null;
+  yDown = null;
+}
+
 const navBtnHandler = () => {
   console.log(DOMElements.nav);
   DOMElements.nav.classList.add("display-nav");
@@ -74,74 +137,83 @@ const closeNavBtnHandler = () => {
   DOMElements.nav.classList.remove("display-nav");
   DOMElements.navIcon.style.display = "flex";
   DOMElements.overlay.style.position = "static";
-  closeAccountNavHandler()
+  closeAccountNavHandler();
 };
 
 const myAccountBtnHandler = () => {
-  console.log(DOMElements.sideNavNav)
-  DOMElements.sideNavNav.style.display = 'none';
-  DOMElements.myAccountNav.classList.add('open-account-nav')
-  DOMElements.nav.style.height = '28rem'
-}
+  console.log(DOMElements.sideNavNav);
+  DOMElements.sideNavNav.style.display = "none";
+  DOMElements.myAccountNav.classList.add("open-account-nav");
+  DOMElements.nav.style.height = "28rem";
+};
 
 const closeAccountNavHandler = () => {
-    console.log('here2');
-    if (DOMElements.mobileNav.contains(DOMElements.myAccountNav)) {
-      DOMElements.mobileNav.replaceChild(DOMElements.pagesNav, DOMElements.myAccountNav);
-      return
-    }
-    DOMElements.sideNavNav.style.display = 'block';
-    DOMElements.myAccountNav.classList.remove('open-account-nav')
-    DOMElements.nav.style.height = '21rem'
-}
+  console.log("here2");
+  if (DOMElements.mobileNav.contains(DOMElements.myAccountNav)) {
+    DOMElements.mobileNav.replaceChild(
+      DOMElements.pagesNav,
+      DOMElements.myAccountNav
+    );
+    return;
+  }
+  DOMElements.sideNavNav.style.display = "block";
+  DOMElements.myAccountNav.classList.remove("open-account-nav");
+  DOMElements.nav.style.height = "21rem";
+};
 
 const mobileNavHandler = () => {
-  DOMElements.mobileNav.style.display = 'block';
-  DOMElements.overlay.style.position = 'fixed';
-}
+  DOMElements.mobileNav.style.display = "block";
+  DOMElements.overlay.style.position = "fixed";
+};
 
 const closeMobileNavHandler = () => {
   // conssole.log("th")
-  DOMElements.mobileNav.style.display = 'none';
-  DOMElements.overlay.style.position = 'static';
-} 
+  DOMElements.mobileNav.style.display = "none";
+  DOMElements.overlay.style.position = "static";
+};
 
 const closeOverlayHandler = () => {
   closeMobileNavHandler();
   closeNavBtnHandler();
-  
-  if (document.body.contains(document.querySelector('#order'))) {
+
+  if (document.body.contains(document.querySelector("#order"))) {
     closeOrderPopup();
   }
-}
+};
 
 myMobileAccountBtnHandler = () => {
-  DOMElements.mobileNav.replaceChild(DOMElements.myAccountNav, DOMElements.pagesNav)
-  DOMElements.myAccountNav.classList.add('mobile-my-account-nav')
-  DOMElements.overlay.style.position = 'fixed';
-}
+  DOMElements.mobileNav.replaceChild(
+    DOMElements.myAccountNav,
+    DOMElements.pagesNav
+  );
+  DOMElements.myAccountNav.classList.add("mobile-my-account-nav");
+  DOMElements.overlay.style.position = "fixed";
+};
 
 DOMElements.navIcon.addEventListener("click", () => {
   if (window.matchMedia("(max-width: 780px)").matches) {
-    mobileNavHandler()
-    console.log('here')
+    mobileNavHandler();
+    console.log("here");
   } else {
-    navBtnHandler()
+    navBtnHandler();
   }
-
 });
 
 // Order button handler
 const orderBtnHandler = (btn) => {
-  console.log(btn)
-  console.log('clicked')
-  
-  const src = {
-    signup: DOMElements.nav.querySelector('.side-nav__register').getAttribute('href'),
-    login: DOMElements.nav.querySelector('.side-nav__login').getAttribute('href')
-  }
+  console.log(btn);
+  console.log("clicked");
 
-  console.log(src.signup, src.login)
+  const src = {
+    signup: DOMElements.nav
+      .querySelector(".side-nav__register")
+      .getAttribute("href"),
+    login: DOMElements.nav
+      .querySelector(".side-nav__login")
+      .getAttribute("href"),
+  };
+
+  console.log(src.signup, src.login);
   const orderElement = `
     <div id="order">
       <div class="logo">
@@ -166,29 +238,27 @@ const orderBtnHandler = (btn) => {
           <a class="cta" href="#">Go to website</a>
       </div>
     </div>
-  `
-  DOMElements.overlay.style.backgroundColor = 'rgba(255, 255, 255, 0.822)'
-  DOMElements.overlay.style.position = 'fixed';
-  console.log(btn.closest('section'))
-  
+  `;
+  DOMElements.overlay.style.backgroundColor = "rgba(255, 255, 255, 0.822)";
+  DOMElements.overlay.style.position = "fixed";
+  console.log(btn.closest("section"));
 
-  btn.closest('section').insertAdjacentHTML('afterbegin', orderElement)
-  const order = document.querySelector('a.cta');
-  order.addEventListener('click', () => {
-    closeOrderPopup()
-  })
-}
+  btn.closest("section").insertAdjacentHTML("afterbegin", orderElement);
+  const order = document.querySelector("a.cta");
+  order.addEventListener("click", () => {
+    closeOrderPopup();
+  });
+};
 
 const closeOrderPopup = () => {
-  document.querySelector('#order').remove();
-  DOMElements.overlay.style.position = 'static';
-  DOMElements.overlay.style.backgroundColor = 'rgba(112, 112, 112, 0.055)'
-}
-
+  document.querySelector("#order").remove();
+  DOMElements.overlay.style.position = "static";
+  DOMElements.overlay.style.backgroundColor = "rgba(112, 112, 112, 0.055)";
+};
 
 window.onload = function () {
   interval = setInterval(showSlide, 3000);
-  
+
   DOMElements.indicators.forEach((el) => {
     el.addEventListener("click", () => {
       currentSlide = el.id;
@@ -197,9 +267,24 @@ window.onload = function () {
   });
 };
 
+DOMElements.carouselContainer.addEventListener(
+  "touchstart",
+  handleTouchStart,
+  false
+);
+DOMElements.carouselContainer.addEventListener(
+  "touchmove",
+  handleTouchMove,
+  false
+);
 DOMElements.closeNavBtn.addEventListener("click", closeNavBtnHandler);
 DOMElements.overlay.addEventListener("click", closeOverlayHandler);
 DOMElements.myAccountBtn.addEventListener("click", myAccountBtnHandler);
-DOMElements.closeAccountNav.addEventListener('click', closeAccountNavHandler)
-DOMElements.mobileNavAccount.addEventListener('click', myMobileAccountBtnHandler)
-DOMElements.orderBtn.forEach(btn => btn.addEventListener('click', orderBtnHandler.bind(null, btn)))
+DOMElements.closeAccountNav.addEventListener("click", closeAccountNavHandler);
+DOMElements.mobileNavAccount.addEventListener(
+  "click",
+  myMobileAccountBtnHandler
+);
+DOMElements.orderBtn.forEach((btn) =>
+  btn.addEventListener("click", orderBtnHandler.bind(null, btn))
+);
