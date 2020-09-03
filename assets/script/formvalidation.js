20dc32bbf41ff0e31717b75db234f0f5f9b3b299
Ahmad Usman <usmanah9817@gmail.com>
2020-09-03 11:49:10 +0100

    complete project

--- assets/script/formvalidation.js
+++ assets/script/formvalidation.js
@@ -1,42 +1,64 @@
+const form = document.querySelector("form");
+
 const formElements = {
-  form: document.querySelector("form"),
+  inputs: form.querySelectorAll("label input"),
+  upload: form.querySelectorAll("label[for='upload']"),
+  select: form.querySelectorAll("select")
 };
-console.log(formElements.form)
-
-const inputs = formElements.form.querySelectorAll("label input");
-
-console.log(inputs);
 
 const checkInput = (input) => {
   console.log(input.type);
-  if (input.value === "" && input.type !== 'file') {
+  if (input.type === 'file' && input.value === "") {
+    return
+  } else if (input.value === "") {
     promptError(input);
   } else {
+    console.log("reached")
     removeErrorMessage(input);
     return;
   }
 };
 
-const promptError = (input) => {
-  if (input.classList.contains("error")) {
-    return;
-  }
-
+const promptError = (input, type) => {
   const errorMessage = `
-        <span>*required</span>
+        <span class="error-text">*required</span>
     `;
-  console.log(input.parentNode);
-  input.classList.add("error");
-  input.parentNode.insertAdjacentHTML("beforeend", errorMessage);
+
+  if (type === "file") {
+    return
+  } else {
+    console.log(input.parentNode);
+    if (input.classList.contains("error")) {
+      return;
+    }
+    input.classList.add("error");
+    input.parentNode.insertAdjacentHTML("beforeend", errorMessage);
+  }
+  
+
 };
 
 const removeErrorMessage = (input) => {
+  console.log(input.type)
+
+  if (input.type === 'file') {
+    return
+  } else {
+    console.log(input.classList.contains("error"))
     if (input.classList.contains("error")) {
+
         input.parentNode.removeChild(input.parentNode.querySelector('span'))
         input.classList.remove("error")
     }
+  }
 }
 
-inputs.forEach((input) => {
+formElements.inputs.forEach((input) => {
   input.addEventListener("blur", checkInput.bind(null, input));
+  input.addEventListener('change', checkInput.bind(null, input))
+});
+
+formElements.select.forEach((select) => {
+  select.addEventListener("blur", checkInput.bind(null, select));
+  select.addEventListener('change', checkInput.bind(null, select))
 });
